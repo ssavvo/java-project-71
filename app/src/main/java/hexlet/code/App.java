7f8fe23776bdcd55d02bf98825cdaaec0a10acda
Savvo Vjacheslav <savvo.vjacheslav@gmail.com>
2023-04-11 21:51:28 +0300

    3 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,10 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
-@Command(name = "checksum", mixinStandardHelpOptions = true, version = "gendiff 0.0.1",
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.0.1",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
     @Override
