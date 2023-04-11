ecc0fc36e5b4fdfdaf8d14b55a0ba1ec18b10f50
Savvo Vjacheslav <savvo.vjacheslav@gmail.com>
2023-04-11 21:50:15 +0300

    3 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "checksum", mixinStandardHelpOptions = true, version = "gendiff 0.0.1",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+
+        return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
