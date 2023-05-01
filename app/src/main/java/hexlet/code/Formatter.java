c8410a4465634747a6294d970cb64a7feebc52c8
Savvo Vjacheslav <savvo.vjacheslav@gmail.com>
2023-05-01 22:58:26 +0300

    10 step

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,15 +1,17 @@
 package hexlet.code;
 
+import hexlet.code.formatters.Json;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Stylish;
 
 import java.util.Map;
 
 public class Formatter {
-    public static String format(Map<String, KeyState> diffMap, String style) {
+    public static String format(Map<String, KeyState> diffMap, String style) throws Exception{
         return switch (style) {
             case "stylish" -> Stylish.format(diffMap);
             case "plain" -> Plain.format(diffMap);
+            case "json" -> Json.toJson(diffMap);
             default -> "";
         };
     }
--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,4 +1,12 @@
 package hexlet.code.formatters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.KeyState;
+
+import java.util.Map;
+
 public class Json {
+    public static String toJson(Map<String, KeyState> diff) throws Exception{
+        return new ObjectMapper().writeValueAsString(diff);
+    }
 }
